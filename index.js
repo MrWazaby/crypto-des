9e56ab8b363428379f1345648c93bc9ab8d223ca
Alexandre Martin <alexandre@m4rtin.xyz>
2017-11-21 11:28:57 +0100

    Split master key

--- index.js
+++ index.js
@@ -20,10 +20,16 @@ message = [
   25, 26, 27, 28, 29, 30, 31, 32,
   33, 34, 35, 36, 37, 38, 39, 40,
   41, 42, 43, 44, 45, 46, 47, 48,
-  49, 50, 51, 52, 53, 54, 55, 56, 
+  49, 50, 51, 52, 53, 54, 55, 56,
   57, 58, 59, 60, 61, 62, 63, 64
 ]
 
+// Step 1
 message = permutations.permutation(message, permutationsTable.initPerm)
 message = permutations.splitMessage(message)
-console.log(permutations.permutation(message[1], expansionTable))
+
+// Step 2
+message[1] = permutations.permutation(message[1], expansionTable)
+masterKey = permutations.splitMessage(masterKey)
+
+console.log(masterKey)
