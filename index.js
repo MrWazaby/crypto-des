4cee8922c04b8ef0be83a9985c77bbc5b507b885
Alexandre Martin <alexandre@m4rtin.xyz>
2017-11-21 09:22:08 +0100

    Add the index and initial table

---
+++ index.js
@@ -0,0 +1,7 @@
+/*
+ * DES Implementation in JavaScript
+ * Efrei Promo 2019 - Majeure Sécurité
+ * Novembre 2017
+ * BOUQUET Julien <julien.bouquet@efrei.net>
+ * MARTIN Alexandre <alexandre.martin@efrei.net>
+ */
