c39b4b35312cb17875debd6a11acef5b3184d43d
Alexandre Martin <alexandre@m4rtin.xyz>
2017-12-10 18:36:37 +0100

    Add reverse perm

--- tables/permutations.js
+++ tables/permutations.js
@@ -109,6 +109,18 @@ permut32 =
   22,	11,	4,	25
 ]
 
+reversePerm =
+[
+  40,	8,	48,	16,	56,	24,	64,	32,
+  39,	7,	47,	15,	55,	23,	63,	31,
+  38,	6,	46,	14,	54,	22,	62,	30,
+  37,	5,	45,	13,	53,	21,	61,	29,
+  36,	4,	44,	12,	52,	20,	60,	28,
+  35,	3,	43,	11,	51,	19,	59,	27,
+  34,	2,	42,	10,	50,	18,	58,	26,
+  33,	1,	41,	9,	49,	17,	57,	25
+]
+
 
 module.exports = {
   initPerm : initPerm,
@@ -117,5 +129,6 @@ module.exports = {
   keyShift: keyShift,
   pc2: pc2,
   sBoxs: sBoxs,
-  permut32: permut32
+  permut32: permut32,
+  reversePerm: reversePerm
 }
